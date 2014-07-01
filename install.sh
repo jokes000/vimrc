ae8260d0ec9a1b4310d922d90bd5e7ecf2cf894c
Francis <lihuiyi0406@gmail.com>
2014-07-01 09:42:12 +0800

    update scripts.

--- install.sh
+++ install.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-CURRENT_DIR=`pwd`
+VIMHOME=$HOME/.vim
 
-lnif() {
-    if [ -e "$1" ]; then
-        ln -sf "$1" "$2"
-    fi
-}
+echo "cloning into vim home"
+git clone https://github.com/jokes000/vimrc.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
 
-echo "Step1: backing up current vim config"
-today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
-
-echo "Step2: download bundles"
-python update_plugins.py
-
-echo "Step2: setting up symlinks"
-lnif $CURRENT_DIR/vimrc $HOME/.vimrc
-lnif "$CURRENT_DIR/" "$HOME/.vim"
-
-echo "Step3: set up font to fix powerline font bug."
-chmod 644 Menlo-Powerline.otf
-sudo mkdir /usr/share/fonts/opentype
-sudo cp Menlo-Powerline.otf /usr/share/fonts/opentype/
-sudo fc-cache -fv
+echo "install vimrc"
+ln -s vimrc "$HOME/.vimrc"
 
 echo "Install Done!"
