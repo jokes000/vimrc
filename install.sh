2f18f6a13b97cf7554f9b2e4f40520fbf148f300
Francis <lihuiyi0406@gmail.com>
2014-07-01 10:08:18 +0800

    major change.

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 VIMHOME=$HOME/.vim
 
-echo "cloning into vim home"
-git clone https://github.com/jokes000/vimrc.git "$VIMHOME"
-cd "$VIMHOME"
-git submodule update --init
+echo "install bundles"
+git clone https://github.com/scrooloose/nerdtree bundle/nerdtree
+git clone https://github.com/Lokaltog/vim-powerline bundle/vim-powerline
+git clone https://github.com/wincent/Command-T bundle/Command-T
 
 echo "install vimrc"
-ln -s vimrc "$HOME/.vimrc"
+cp vimrc "$HOME/.vimrc"
+
+echo "update font for powerline"
+sudo mkdir /usr/share/fonts/opentype
+sudo cp Menlo-Powerline.otf /usr/share/fonts/opentype/Menlo-Powerline.otf
+sudo fc-cache -fv
 
 echo "Install Done!"
